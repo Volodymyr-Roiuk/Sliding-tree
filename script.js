313845911494909da28db9e2188edc39e021102d
Volodymyr_Roiuk <heppivova@gmail.com>
2019-10-14 20:11:42 +0300

    Solution

---
+++ script.js
@@ -0,0 +1,35 @@
+class SiblingTree {
+  constructor() {
+    this.objArr = [];
+  }
+
+  clickEvent(event) {
+    let ulList = document.querySelector('.tree');
+    let item = event.target.closest('.tree__item');
+    
+    if (!item || !ulList.contains(item)) {
+      return;
+    }
+
+    let findResult = this.objArr.filter(obj => obj.propertyName === item.innerHTML)
+
+    if (findResult.length === 0) {
+      let propertyName = item.innerHTML;
+      let html = item.parentElement.innerHTML;
+      this.objArr.push({propertyName, html});
+      item.parentElement.innerHTML = `<span class="tree__item">${item.innerHTML}</span>`;
+    } else {
+      item.parentElement.innerHTML = findResult[0].html;
+      let index = this.objArr.findIndex(obj => obj.propertyName === findResult[0].propertyName);
+      this.objArr.splice(index, 1);
+    } 
+  }
+
+  addEvents() {
+    let list = document.querySelector('.tree');
+    list.addEventListener('click', this.clickEvent.bind(this));
+  }
+}
+
+let s = new SiblingTree();
+s.addEvents();
